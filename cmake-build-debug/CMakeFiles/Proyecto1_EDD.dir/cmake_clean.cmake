21a20b38189ad245a4582efa46974d8995634b30
Cristian Caceres <crisgame77@gmail.com>
2020-09-03 21:29:21 -0600

    Primera prueba usando Git

---
+++ cmake-build-debug/CMakeFiles/Proyecto1_EDD.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Proyecto1_EDD.dir/main.cpp.obj"
+  "Proyecto1_EDD.exe"
+  "Proyecto1_EDD.exe.manifest"
+  "Proyecto1_EDD.pdb"
+  "libProyecto1_EDD.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Proyecto1_EDD.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std; //se pone para usar el cout
+
+int main() {
+    cout << "***************************************************************"<<endl; //endl da un salto de linea
+    cout << "* USAC                                                        *"<<endl;
+    cout << "* Estructura de datos                                         *"<<endl;
+    cout << "* Seccion A                                                   *"<<endl;
+    cout << "* Cristian Jonatan Caceres Gonzalez                           *"<<endl;
+    cout << "* 201801183                                                   *"<<endl;
+    cout << "***************************************************************"<<endl;
+    system("pause"); //Para detectar teclazo
+
+    cout << "1.Ver Proyectos"<<endl;
+    cout << "2.Editar Proyectos"<<endl;
+    cout << "3.Cargar Proyecto"<<endl;
+    cout << "4.Graficar Proyectos"<<endl;
+    cout << "5.Guardar Proyectos"<<endl;
+    cout << "6.Cargar Librerias"<<endl;
+    cout << "7.Salir"<<endl;
+    cout << "Elija una opcion: ";
+
+    int opc; //Declarando variable opcion
+    cin>>opc; //Para recibir la variable opc
+
+    while (opc!=7); { //Este es un ciclo para repetir algo
+
+        switch (opc) { //Aqui se coloca el nombre de la variable de opciones
+            case 1:
+                cout << "Usted abrio la opcion Ver proyectos";
+                return 0;
+            case 2:
+                cout << "Usted abrio la opcion Editar Proyectos";
+                return 0;
+            case 3:
+                cout << "Usted abrio la opcion Cargar proyectos";
+                return 0;
+            case 4:
+                cout << "Usted abrio la opcion Graficar proyectos";
+                return 0;
+            case 5:
+                cout << "Usted abrio la opcion Guardar proyectos";
+                return 0;
+            case 6:
+                cout << "Usted abrio la opcion Cargar proyectos";
+                return 0;
+            case 7:
+                cout << "Esperamos volverlo a ver";
+                break;
+        }
+    }
+
+
+    return 0;
+
+}
